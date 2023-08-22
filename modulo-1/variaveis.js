fa7c460f4bb0147b6f82bab359cf02d91499af25
Misael Sena <misaellsena@gmail.com>
2023-08-22 18:02:11 -0300

    Conclusão do Exercício sobre criação de variáveis

---
+++ modulo-1/variaveis.js
@@ -0,0 +1,29 @@
+let booleana;
+let bigint;
+let string;
+let symbol;
+
+console.log(booleana);
+console.log(bigint);
+console.log(string);
+console.log(symbol);
+
+booleana = null;
+bigint = null;
+string = null;
+symbol = null;
+
+console.log(booleana);
+console.log(bigint);
+console.log(string);
+console.log(symbol);
+
+booleana = true;
+bigint = BigInt(9007199254740991436256246264);
+string = "Misael";
+symbol = Symbol('Andreza');
+
+console.log(booleana);
+console.log(bigint);
+console.log(string);
+console.log(symbol);

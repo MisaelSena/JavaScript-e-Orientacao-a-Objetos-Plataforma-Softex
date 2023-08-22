03ab804fb736b97df1268dfa06aeade5e4659195
Misael Sena <misaellsena@gmail.com>
2023-08-22 18:11:02 -0300

    Ćonclusão do Exercício de variáveis de cadastro de pessoa

---
+++ modulo-1/cadastro-de-pessoa.js
@@ -0,0 +1,14 @@
+let nome; //String
+let salario;//Number
+let idade;//Number
+let possuiDiploma;//Boolean
+
+nome = "Misael";
+salario = 30000;
+idade = 32;
+possuiDiploma = true;
+
+console.log("Nome: ",nome);
+console.log("Salário: ",salario);
+console.log("Idade: ",idade);
+console.log("Pussui Diploma: ",possuiDiploma);

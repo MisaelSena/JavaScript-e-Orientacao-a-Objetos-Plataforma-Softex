41f7a2228a04a59f93d6554a3cdf274c1d295fa7
Misael Sena <misaellsena@gmail.com>
2023-08-27 11:17:05 -0300

    Calculadora da média de 3 notas criada. Página html adicionada apenas para execução do html no navegador

---
+++ modulo-1/calculadora-de-media.js
@@ -0,0 +1,9 @@
+const nota1 = parseFloat(prompt("Informe a Nota 1:"));
+const nota2 = parseFloat(prompt("Informe a Nota 2:"));
+const nota3 = parseFloat(prompt("Informe a Nota 3:"));
+
+const media = (nota1+nota2+nota3)/3;
+
+console.log("A média das notas informadas é: ",media);
+
+

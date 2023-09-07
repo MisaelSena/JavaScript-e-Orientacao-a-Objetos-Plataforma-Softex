0e452d58f0c023ca1dc42a1e1bc49749f1972b2e
Misael Sena <misaellsena@gmail.com>
2023-09-07 13:33:59 -0300

    Criação do objeto Banco com seus atributos e métodos

--- modulo-4/banco-simples.js
+++ modulo-4/banco-simples.js
@@ -26,6 +26,6 @@ let conta = new Banco(123, 1000, "CC","132-5");
 
 console.log("Conta: "+conta.numConta()+" Saldo Inicial: "+conta.buscarSaldo());
 conta.deposito(100);
-console.log("Saldo Após Depósito: "+conta.buscarSaldo());
+console.log("Conta: "+conta.numConta()+" Saldo Após Depósito: "+conta.buscarSaldo());
 conta.saque(40);
-console.log("Saldo Após Saque: "+conta.buscarSaldo());
+console.log("Conta: "+conta.numConta()+" Saldo Após Saque: "+conta.buscarSaldo());

bbcdaa57a782b26fd94a700fee06c46832b79fe3
Misael Sena <misaellsena@gmail.com>
2023-09-10 12:57:25 -0300

    Criação da verificação de número Par ou Ímpar

--- modulo-5/verifica-par-ou-impar.js
+++ modulo-5/verifica-par-ou-impar.js
@@ -1,8 +1,11 @@
-const numero = 7;
-const modulo = numero%2;
-
-if (modulo===0) {
-    console.log(`O número ${numero} é par!`);
-} else {
-    console.log(`O número ${numero} é ímpar!`);
+function verificaParOuImpar(num) {
+    const numero = num;
+    const modulo = numero%2;
+    if (modulo===0) {
+        console.log(`O número ${numero} é par!`);
+    } else {
+        console.log(`O número ${numero} é ímpar!`);
+    }
 }
+
+verificaParOuImpar(7);
